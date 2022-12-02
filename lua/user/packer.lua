7c237ebfc876fab2eca8cf1c6f6cde1c58cb2fa8
AyonC <ayonc@github.com>
2022-12-02 16:45:26 +0530

    done

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,4 +1,5 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require('plugins')` from your init.vimpacker
+
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
@@ -10,42 +11,42 @@ return packer.startup(function(use)
 
 	-- Utils plugins
 	use("nvim-lua/plenary.nvim")
-	use('nvim-tree/nvim-web-devicons')
+	use("nvim-tree/nvim-web-devicons")
 
 	-- Telescope
 	use("nvim-telescope/telescope.nvim")
 
 	-- Lualine
-	use('nvim-lualine/lualine.nvim')
+	use("nvim-lualine/lualine.nvim")
 
 	-- Treesitter
 	use({
-		'nvim-treesitter/nvim-treesitter',
-		run=':TSUpdate'
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
 	})
-	use('nvim-treesitter/nvim-treesitter-context')
-	use('windwp/nvim-ts-autotag')
-	use('p00f/nvim-ts-rainbow')
-	use('windwp/nvim-autopairs')
+	use("nvim-treesitter/nvim-treesitter-context")
+	use("windwp/nvim-ts-autotag")
+	use("p00f/nvim-ts-rainbow")
+	use("windwp/nvim-autopairs")
 
 	-- Nvimtree
-	use('nvim-tree/nvim-tree.lua')
+	use("nvim-tree/nvim-tree.lua")
 
 	-- Indentline
 	use("lukas-reineke/indent-blankline.nvim")
 
 	-- Cmp
-  use "hrsh7th/nvim-cmp"
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "saadparwaiz1/cmp_luasnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
 
-  -- snippets
-  use "L3MON4D3/LuaSnip"
-	use "rafamadriz/friendly-snippets"
+	-- snippets
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
 	-- Lsp
 	use("neovim/nvim-lspconfig")
--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,45 +1,44 @@
 -- Requires
-local M = require('user.keymap')
+local M = require("user.keymap")
 
 -- Variables
 local inoremap = M.inoremap
 local nnoremap = M.nnoremap
 local vnoremap = M.vnoremap
-local keymap = vim.api.nvim_set_keymap
 
 -- Nvimtree
-nnoremap('<leader>e', '<cmd>NvimTreeToggle<CR>')
+nnoremap("<leader>e", "<cmd>NvimTreeToggle<CR>")
 
 -- Telescope
-nnoremap('<C-p>', '<cmd>Telescope find_files<CR>')
-nnoremap('<leader>fb', '<cmd>Telescope buffers<CR>')
-nnoremap('<leader>fw', '<cmd>Telescope grep_string<CR>')
+nnoremap("<C-p>", "<cmd>Telescope find_files<CR>")
+nnoremap("<leader>fb", "<cmd>Telescope buffers<CR>")
+nnoremap("<leader>fw", "<cmd>Telescope grep_string<CR>")
 
 -- Split switch
-nnoremap('<C-h>', '<C-w>h')
-nnoremap('<C-j>', '<C-w>j')
-nnoremap('<C-k>', '<C-w>k')
-nnoremap('<C-l>', '<C-w>l')
+nnoremap("<C-h>", "<C-w>h")
+nnoremap("<C-j>", "<C-w>j")
+nnoremap("<C-k>", "<C-w>k")
+nnoremap("<C-l>", "<C-w>l")
 
 -- Split resize
-nnoremap('<C-left>', '<cmd>vertical resize -2<CR>')
-nnoremap('<C-right>', '<cmd>vertical resize +2<CR>')
-nnoremap('<C-up>', '<cmd>resize -2<CR>')
-nnoremap('<C-down>', '<cmd>resize +2<CR>')
+nnoremap("<C-left>", "<cmd>vertical resize -2<CR>")
+nnoremap("<C-right>", "<cmd>vertical resize +2<CR>")
+nnoremap("<C-up>", "<cmd>resize -2<CR>")
+nnoremap("<C-down>", "<cmd>resize +2<CR>")
 
 -- Normal mode
 nnoremap("<A-j>", "<cmd>:m .+1<CR>==gi<ESC>")
 nnoremap("<A-k>", "<cmd>:m .-2<CR>==gi<ESC>")
 
 -- Insert mode
-inoremap('jk', '<ESC>')
-inoremap('Jk', '<ESC>')
-inoremap('jK', '<ESC>')
-inoremap('JK', '<ESC>')
+inoremap("jk", "<ESC>")
+inoremap("Jk", "<ESC>")
+inoremap("jK", "<ESC>")
+inoremap("JK", "<ESC>")
 
 --Visual Mode indent
-vnoremap('>', '>gv')
-vnoremap('<', '<gv')
+vnoremap(">", ">gv")
+vnoremap("<", "<gv")
 
 -- Move text up and down in Visual Mode
 vnoremap("<A-j>", ":m .+2<CR>gv")
